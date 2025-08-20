d434c1bcf2d92c5591c13061be79159e8ca24ef7
OsirizF <226719519+OsirizF@users.noreply.github.com>
2025-08-20 14:54:30 -0300

    Update Nice_Monkey.user.js

--- Nice_Monkey.user.js
+++ Nice_Monkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nice_Monkey
 // @namespace    http://tampermonkey.net/
-// @version      3.3
+// @version      5.3
 // @description  that's all folks!
 // @author       almaviva.fpsilva
 // @match        https://cxagent.nicecxone.com/home*
