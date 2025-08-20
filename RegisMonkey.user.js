da8b5ed7589a16a30a0149f0b3dc9a85b3404881
OsirizF <226719519+OsirizF@users.noreply.github.com>
2025-08-20 16:30:18 -0300

    Update RegisMonkey.user.js

--- RegisMonkey.user.js
+++ RegisMonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RegisMonkey
 // @namespace    http://tampermonkey.net/
-// @version      5
+// @version      6
 // @description  try to take over the world!
 // @author       You
 // @match        https://smileshelp.zendesk.com/agent/*
