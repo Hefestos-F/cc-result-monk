8640c3b86dfcf8e94324f86d47cc432a32ef5831
Fran <226719519+Hefestos-F@users.noreply.github.com>
2025-11-17 10:53:35 -0300

    up

--- Nice_Monkey.user.js
+++ Nice_Monkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nice_Monkey
 // @namespace    https://github.com/Hefestos-F/cc-result-monk
-// @version      3.5.1.2
+// @version      3.5.1.3
 // @description  that's all folks!
 // @author       almaviva.fpsilva
 // @match        https://cxagent.nicecxone.com/home*
